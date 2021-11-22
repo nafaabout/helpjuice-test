f3c1e38fa29643dd542875f23ca9434b3caf0fa3
Nafaa Boutefer <nafaabout@gmail.com>
2021-11-22 16:48:27 +0100

    Fix turbo-rails requiring redirect on form error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
     else
       flash[:alert] = 'Wrong username or password'
 
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
