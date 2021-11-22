1ce8b31bcca7281ccbdf6d9774c022d702bf89a9
Nafaa Boutefer <nafaabout@gmail.com>
2021-11-22 16:55:54 +0100

    Remove non used code

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,10 +5,4 @@ class Search < ApplicationRecord
 
   belongs_to :user
   enum status: %i[incomplete complete]
-
-  def updated_since?(time)
-    raise ArgumentError, ':time must be an ActiveSupport::TimeWithZone' unless time.is_a?(ActiveSupport::TimeWithZone)
-
-    updated_at > time
-  end
 end
